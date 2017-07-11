300e893cfa3a598bf33e6f9b21fa43845f8a0771
Nathan Fisher <nfisher@junctionbox.ca>
2017-07-12 01:42:38 +0200

    Update SDK version.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   (defproject lein-eb version
     :description "Leiningen plugin to manage Amazon's Elastic Beanstalk"
     :url "https://github.com/nfisher/lein-eb"
-    :dependencies [[com.amazonaws/aws-java-sdk-elasticbeanstalk "1.10.77" :exclusions [commons-logging]]
-                   [com.amazonaws/aws-java-sdk-s3 "1.10.77" :exclusions [commons-logging]]]
+    :dependencies [[com.amazonaws/aws-java-sdk-elasticbeanstalk "1.11.160" :exclusions [commons-logging]]
+                   [com.amazonaws/aws-java-sdk-s3 "1.11.160" :exclusions [commons-logging]]]
     :min-lein-version "2.0.0"
     :eval-in-leiningen true))
